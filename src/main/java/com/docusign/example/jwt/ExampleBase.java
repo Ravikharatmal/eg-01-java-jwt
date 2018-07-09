712b576ce706a2074dfaa9510cdb9e933c2165ab
Shadi Massalha <shady.massalha@gmail.com>
2018-07-09 16:00:01 +0300

    - update account once - calculate time buffer on check

--- src/main/java/com/docusign/example/jwt/ExampleBase.java
+++ src/main/java/com/docusign/example/jwt/ExampleBase.java
@@ -17,20 +17,27 @@ public class ExampleBase {
     private static final long TOKEN_EXPIRATION_IN_SECONDS = 3600;
     private static final long TOKEN_REPLACEMENT_IN_MILLISECONDS = 10*60*1000;
 
+    private static OAuth.UserInfo.Account _account;
     private static File privateKeyTempFile = null;
     private static long expiresIn;
     private static String _token = null;
     protected final ApiClient apiClient;
-    protected static String accountID;
+
+    protected static String getAccountId() {
+        return _account.getAccountId();
+    };
+
+
 
     public ExampleBase(ApiClient apiClient) throws IOException {
         privateKeyTempFile = DSHelper.createPrivateKeyTempFile("private-key");
         this.apiClient =  apiClient;
     }
 
-    protected void validateToken() throws IOException, ApiException {
+    protected void checkToken() throws IOException, ApiException {
 
-        if(this._token == null || System.currentTimeMillis() > this.expiresIn) {
+        if(this._token == null
+                || (System.currentTimeMillis() + TOKEN_REPLACEMENT_IN_MILLISECONDS) > this.expiresIn) {
             updateToken();
         }
     }
@@ -43,16 +50,17 @@ public class ExampleBase {
                 DSConfig.CLIENT_ID,
                 DSConfig.IMPERSONATED_USER_GUID,
                 TOKEN_EXPIRATION_IN_SECONDS);
-        //TODO: this is work around to fix incorrect set basePath account.
+        //TODO: this is work around to fix incorrect set basePath account
         apiClient.setBasePath(null);
-        OAuth.UserInfo.Account acct = this.getAccountInfo(apiClient);
+
+        if(_account == null)
+            _account = this.getAccountInfo(apiClient);
         // default or configured account id.
-        accountID = acct.getAccountId();
-        apiClient.setBasePath(acct.getBaseUri() + "/restapi");
+        apiClient.setBasePath(_account.getBaseUri() + "/restapi");
 
         _token = apiClient.getAccessToken();
         //notice that expiresIn value is not exposed yet by the SDK so, we will assume it as 1 hours.
-        expiresIn = System.currentTimeMillis() + (TOKEN_EXPIRATION_IN_SECONDS * 1000) - TOKEN_REPLACEMENT_IN_MILLISECONDS;
+        expiresIn = System.currentTimeMillis() + (TOKEN_EXPIRATION_IN_SECONDS * 1000);
     }
 
     private OAuth.UserInfo.Account getAccountInfo(ApiClient client) throws ApiException {
@@ -60,6 +68,7 @@ public class ExampleBase {
         OAuth.UserInfo userInfo = client.getUserInfo(client.getAccessToken());
         OAuth.UserInfo.Account accountInfo = null;
 
+        //TODO: fix the order of check
         if(DSConfig.TARGET_ACCOUNT_ID == null || DSConfig.TARGET_ACCOUNT_ID.length() == 0){
             List<OAuth.UserInfo.Account> accounts = userInfo.getAccounts();
 
